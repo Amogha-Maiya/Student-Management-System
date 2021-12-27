52ad7cef5fc5de2cb55183dbf748882904b644ec
Akash Rao <rao28271@gmail.com>
2021-12-27 13:15:39 +0530

    about us font

--- admin/aboutus.php
+++ admin/aboutus.php
@@ -1,45 +1,47 @@
 <html>
+
 <head>
-    <title>About</title>
-<link rel="stylesheet" href="../css/aboutus.css?v=<?php echo time(); ?>"" type="text/css">
-<!-- <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet"> -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"> -->
+  <title>About</title>
+  <link rel="stylesheet" href="../css/aboutus.css?v=<?php echo time(); ?>"" type=" text/css">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet"> -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet"><!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"> -->
 
 </head>
-<body background = "write.jpg" style="background-size: cover;">
-    <header>
-      <nav>
-        <div class="row clearfix" >
-            <ul class="main-nav" animate slideInDown>
-                <li><a href="../index.php">HOME</a></li>
-                <li><a href="aboutus.php">ABOUT</a></li>
-                <li><a href="contactus.php">CONTACT</a></li>
-                <li><a href="../login.php">ADMIN LOGIN</a></li>
-          </ul>
-        </div>
-      </nav>
-      <div class="main-content-header">
-          <h2 class="search" style="color: cyan">Student Management System</h2>
-             <div class="div2">
-             
-             <p>Student Management System is software which is helpful for students as
-                well as the school authorities.
-                 In the current system all the activities are done manually. 
-                 It is very time consuming and costly. In the Software we can register 
-                 as a student or as an administrator. An administrator has the power to add
-                  new users, edit and can also delete a user. 
-                  The student is able to view his/her information.
-            </p>
-             <p>  
-                Achieving this objective is difficult using the manual system as the information is scattered, can be redundant, and collecting relevant information may be very time-consuming. It reduces the manpower required. It always provides accurate information.
-                All gathered and extra information can be saved and can be accessed at any time. All the users can get the desired information without delay.
-            </p>
-             </div>  
-          </div>
-    </header>
-    
+
+<body background="write.jpg" style="background-size: cover;">
+  <header>
+    <nav>
+      <div class="row clearfix">
+        <ul class="main-nav" animate slideInDown>
+          <li><a href="../index.php">HOME</a></li>
+          <li><a href="aboutus.php">ABOUT</a></li>
+          <li><a href="contactus.php">CONTACT</a></li>
+          <li><a href="../login.php">ADMIN LOGIN</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="main-content-header">
+      <h2 class="search" style="color: cyan">Student Management System</h2>
+      <div class="div2">
+
+        <p>Student Management System is software which is helpful for students as
+          well as the school authorities.
+          In the current system all the activities are done manually.
+          It is very time consuming and costly. In the Software we can register
+          as a student or as an administrator. An administrator has the power to add
+          new users, edit and can also delete a user.
+          The student is able to view his/her information.
+        </p>
+        <p>
+          Achieving this objective is difficult using the manual system as the information is scattered, can be redundant, and collecting relevant information may be very time-consuming. It reduces the manpower required. It always provides accurate information.
+          All gathered and extra information can be saved and can be accessed at any time. All the users can get the desired information without delay.
+        </p>
+      </div>
+    </div>
+  </header>
+
 </body>
+
 </html>
