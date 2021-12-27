e7dadac720ebcbd6ba92ce3128fab9a0a41ad053
Akash Rao <rao28271@gmail.com>
2021-12-27 13:12:56 +0530

    all details font

--- admin/allstudentdata.php
+++ admin/allstudentdata.php
@@ -13,15 +13,15 @@ if (isset($_SESSION['uid'])) {
 <head>
   <title>Student Details</title>
   <link rel="stylesheet" href="../css/allstudentdata.css" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-
+  <!-- <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet"> -->
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"> -->
+  <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
 </head>
 
 <body>
   <header>
     <nav>
-      <div class="row clearfix">
+      <div class="row clearfix" style="font-size: 26px;">
         <ul class="main-nav" animate slideInDown>
           <li><a href="../index.php"><b>HOME</b></a></li>
           <li><a href="aboutus.php"><b>ABOUT</b></a></li>
@@ -32,18 +32,18 @@ if (isset($_SESSION['uid'])) {
       </div>
     </nav>
     <div class="main-content-header">
-      <h2>Student Record</h2>
+      <h2 style="font-size: 35px;">Student Record</h2>
       <form>
-        <table>
+        <table style="font-size: 15px; margin-right:200px;">
           <tr>
-            <th class="id_h1">Id </th>
-            <th class="name_h1">Name </th>
-            <th class="contact_h1">Class </th>
-            <th class="contact_h1">Roll No</th>
-            <th class="contact_h1">Father Name</th>
-            <th class="massage_h1">Mother Name </th>
-            <th class="contact_h1">Village</th>
-            <th class="name_h1">Mobile No</th>
+            <th class="id_h1" style="font-size: 25px;">Id </th>
+            <th class="name_h1" style="font-size: 25px;">Name </th>
+            <th class="contact_h1" style="font-size: 25px;">Class </th>
+            <th class="contact_h1" style="font-size: 25px;">Roll No</th>
+            <th class="contact_h1" style="font-size: 25px;">Father Name</th>
+            <th class="massage_h1" style="font-size: 25px;">Mother Name </th>
+            <th class="contact_h1" style="font-size: 25px;">Village</th>
+            <th class="name_h1" style="font-size: 25px;">Mobile No</th>
           </tr>
 
           <?php
